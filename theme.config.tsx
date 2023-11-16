08cf4635ce23d836c4d3dd3ee66d1b8582facf8c
dicechick373 <dicechick373@gmail.com>
2023-11-16 11:31:48 +0900

    test

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,15 +4,15 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>doboku-note</span>,
   darkMode: false,
-  project: {
-    link: "https://github.com/shuding/nextra-docs-template",
-  },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  // project: {
+  //   link: "https://github.com/shuding/nextra-docs-template",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Nextra Docs Template",
+    text: "doboku-note.com",
   },
 };
 
