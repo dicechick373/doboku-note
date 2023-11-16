82e6b81ff304a2976413bb888f049f1b9a5cf358
dicechick373 <dicechick373@gmail.com>
2023-11-16 10:08:13 +0900

    test

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://doboku-note.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
