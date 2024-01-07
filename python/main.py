6270f1f8ecff954f698d8e7b18ae6348cc2b8cd4
dicechick373 <dicechick373@gmail.com>
2024-01-07 17:34:11 +0900

    test

--- python/main.py
+++ python/main.py
@@ -114,7 +114,7 @@ def convert(file):
 
 if __name__ == "__main__":
 
-    file = "pages/load/road-management/road-construction/acquisition-of-land/government-property.mdx"
+    file = "pages/load/road-management/road-construction/acquisition-of-land/propertyownership.mdx"
     
     convert(file)
         
